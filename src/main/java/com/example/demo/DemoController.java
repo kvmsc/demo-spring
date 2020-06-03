a3f243f888bf150a4a003f5a09614568e7c71164
Mohit Chandra <kvmsc@github.com>
2020-06-03 19:56:43 +0530

    Created Demo App with tests

---
+++ src/main/java/com/example/demo/DemoController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class DemoController {
+    @RequestMapping("/")
+    public String index() {
+        return "Hello World in Spring Boot!!";
+    }
+}
